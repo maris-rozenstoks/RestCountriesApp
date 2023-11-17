eb393170d8ca5f3bdae60553b4be62fa098d29e4
maris-rozenstoks <146951342+maris-rozenstoks@users.noreply.github.com>
2023-11-17 12:59:27 +0200

    Initial Commit

---
+++ RestCountriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestCountriesApp
+//
+//  Created by maris.rozenstoks on 17/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
